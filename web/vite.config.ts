64cdf29c5f91aa5437ce3cb8247f4ba6837b76f4
vicanso <tree.xie@outlook.com>
2024-09-21 21:38:15 +0800

    fix: adjust base path for static serve

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -14,6 +14,7 @@ function manualChunks(id: string) {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
