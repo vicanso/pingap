df8bb50c1f1eaca8b8c2e8c07f8735a6003c2452
vicanso <tree.xie@outlook.com>
2025-01-25 11:03:40 +0800

    docs: update copyright

--- src/util/ip.rs
+++ src/util/ip.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 Tree xie.
+// Copyright 2024-2025 Tree xie.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
