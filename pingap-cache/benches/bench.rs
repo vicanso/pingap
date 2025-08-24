d4f698e8f4bda4bd8c1413e9b8ba1416647b6352
vicanso <tree.xie@outlook.com>
2025-08-24 16:51:58 +0800

    test: bench for tinyufo

--- pingap-cache/benches/bench.rs
+++ pingap-cache/benches/bench.rs
@@ -1,11 +1,8 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use pingap_core::TinyUfo;
-use std::sync::Arc;
 
 fn bench_tinyufo_get(c: &mut Criterion) {
-    let mut group = c.benchmark_group("tinyufo");
-
-    group.bench_function("tinyufo string", |b| {
+    c.bench_function("tinyufo get", |b| {
         let cache = TinyUfo::new(1000, 1000);
         let key = "key";
         cache.put(key.to_string(), "value", 100);
@@ -13,17 +10,6 @@ fn bench_tinyufo_get(c: &mut Criterion) {
             let _ = cache.get(&key.to_string());
         });
     });
-
-    group.bench_function("tinyufo arc", |b| {
-        let cache = TinyUfo::new(1000, 1000);
-        let key = Arc::new("key");
-        cache.put(key.clone(), "value", 100);
-        b.iter(|| {
-            let _ = cache.get("key");
-        });
-    });
-
-    group.finish();
 }
 
 criterion_group!(benches, bench_tinyufo_get);
